72bc5292a5347979e24a4a8a83ff262c504d2bc1
guangbochen <steven.guangbo.chen@gmail.com>
2013-08-31 02:45:13 +1000

    complete new version of PIA Website

--- js/app.js
+++ js/app.js
@@ -3,3 +3,19 @@ $("#myServicies").mouseenter(function(){
 }).mouseleave(function(){
     $("#myServicies").removeClass('open');
 });
+
+// Smooth Scrolling To Internal Links With jQuery
+// $(document).ready(function(){
+//         $('a[href^="#"]').on('click',function (e) {
+//             e.preventDefault();
+//             var target = this.hash,
+//             $target = $(target);
+//             $('html, body').stop().animate({
+//                 'scrollTop': $target.offset().top
+//             }, 900, 'swing', function () {
+//                 window.location.hash = target;
+//             });
+//         });
+// });
+
+$('.carousel').carousel();
